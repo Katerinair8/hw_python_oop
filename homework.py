911d8aaeb588698485ca161a4f8127a78e897e65
Екатерина Василевич <katerinair8@mail.ru>
2022-06-11 22:49:50 +0300

    правки 3

--- homework.py
+++ homework.py
@@ -42,8 +42,8 @@ class Training:
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        raise NotImplementedError(
-            'Определите метод get_spent_calories в %s.' % (self.__class__.__name__))
+        raise NotImplementedError('Определите метод get_spent_calories в %s.'
+                                  % (self.__class__.__name__))
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
